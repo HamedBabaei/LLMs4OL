a14145ca0014f2b33c673398ca5842a46e87b5f9
Hamed Babaei <hamedbabaeigiglou@gmail.com>
2023-01-12 11:21:12 +0100

    [delete] everything about fb15k dataset from codes

--- configuration/config.py
+++ configuration/config.py
@@ -30,33 +30,11 @@ class BaseConfig:
         self.parser.add_argument("--processed_entity_valid", type=str, default=f"datasets/{dataset}/processed{self.version}/entity_valid.csv")
         self.parser.add_argument("--relations_to_ignore", type=list, default=["_also_see"])
         
-    def add_fb15k_237(self):
-        dataset = "FB15K-237"
-        self.parser.add_argument("--raw_train", type=str, default=f"datasets/{dataset}/raw/train.txt")
-        self.parser.add_argument("--raw_test", type=str, default=f"datasets/{dataset}/raw/test.txt")
-        self.parser.add_argument("--raw_valid", type=str, default=f"datasets/{dataset}/raw/valid.txt")
-        self.parser.add_argument("--freebase_dumps_dir", type=str, default=f"assets/FreeBase/FreeBase_FB15KBased_Dumps")
-        self.parser.add_argument("--freebase_hierarchy", type=str, default=f"assets/FreeBase/freebase_llms2ol_hierarchy.json")
-        self.parser.add_argument("--freebase_types", type=str, default=f"assets/FreeBase/freebaseTypes.tsv")
-        
-        
-        self.parser.add_argument("--processed_train_rel", type=str, default=f"datasets/{dataset}/processed{self.version}/train_rel.csv")
-        self.parser.add_argument("--processed_test_rel", type=str, default=f"datasets/{dataset}/processed{self.version}/test_rel.csv")
-        self.parser.add_argument("--processed_valid_rel", type=str, default=f"datasets/{dataset}/processed{self.version}/valid_rel.csv")
-
-        self.parser.add_argument("--processed_train_ent", type=str, default=f"datasets/{dataset}/processed{self.version}/train_ent.csv")
-        self.parser.add_argument("--processed_test_ent", type=str, default=f"datasets/{dataset}/processed{self.version}/test_ent.csv")
-        self.parser.add_argument("--processed_valid_ent", type=str, default=f"datasets/{dataset}/processed{self.version}/valid_ent.csv")
-
-        self.parser.add_argument("--processed_wordnet_taxonomy", type=str, 
-                                    default=f"datasets/{dataset}/processed{self.version}/processed_wordnet_taxonomy.csv")
-
     def add_geoname(self):
         dataset = "Geonames"
         self.parser.add_argument("--feature_codes", type=str, default=f"datasets/{dataset}/raw/featureCodes_en.txt")
         self.parser.add_argument("--all_countries", type=str, default=f"datasets/{dataset}/raw/allCountries.txt")
         self.parser.add_argument("--depth", type=int, default=3)
-
         self.parser.add_argument("--processed_feature_codes", type=str, default=f"datasets/{dataset}/processed/featureCodes_en.csv")
         self.parser.add_argument("--processed_all_countries", type=str, default=f"datasets/{dataset}/processed/allCountries.csv")
 
@@ -67,9 +45,8 @@ class BaseConfig:
         """
         if db_name == "wn18rr":
             self.add_wnn18rr()
-        if db_name == "fb15k-237":
-            self.add_fb15k_237()
         if db_name == "geonames":
             self.add_geoname()
+
         self.parser.add_argument("-f")
         return self.parser.parse_args()
