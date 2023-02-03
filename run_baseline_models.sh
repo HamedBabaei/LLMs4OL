821d4ed86bb259739a461e765de95dc8fd875cab
Hamed Babaei <hamedbabaeigiglou@gmail.com>
2023-02-03 15:47:02 +0100

    [refactor] shell script

--- run_baseline_models.sh
+++ run_baseline_models.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Enter dataset out of (wn18rr, geonames, nci):"
-read db_name
+read kb_name
 
-echo "Enter output logs path for $db_name results:"
+echo "Enter output logs path for $kb_name results:"
 read log
 
 echo "Enter the name of model out of (bert_large):"
@@ -13,10 +13,9 @@ exec > $log
 echo "Running baseline models!"
 
 templates=("template-1" "template-2" "template-3" "template-4" "template-5" "template-6" "template-7" "template-8")
-
 for template in "${templates[@]}"; do
   echo "Running $model_name for template: $template!"
-  python3 run_baseline_models.py --db_name=$db_name --model_name=$model_name --template=$template
+  python3 run_baseline_models.py --kb_name=$kb_name --model_name=$model_name --template=$template
   echo "Inference for  $model_name on template: $template  is done"
   echo "-----------------------------------------------------------"
 done
