2b74cf94f471bbc193d1b6b332d7edd256ba7ff4
Hamed Babaei <hamedbabaeigiglou@gmail.com>
2023-02-03 15:46:44 +0100

    [refactor] bs model

--- run_baseline_models.py
+++ run_baseline_models.py
@@ -1,6 +1,6 @@
 from configuration import BaseConfig
 from datahandler import DataReader, DataWriter
-from src import WNDataset
+from src import BaselineInferenceDatasetFactory
 from src import BaselineFactory
 from torch.utils.data import DataLoader
 from tqdm import tqdm
@@ -21,24 +21,25 @@ def inference(model, dataloader):
 if __name__=="__main__":
         
     parser = argparse.ArgumentParser()
-    parser.add_argument("--db_name", required=True)
+    parser.add_argument("--kb_name", required=True)
     parser.add_argument("--model_name", required=True)
     parser.add_argument("--template", required=True)
     args = parser.parse_args()
    
     print("args:", args)
-    config = BaseConfig(version=2).get_args(db_name=args.db_name, model=args.model_name, template=args.template)
+    config = BaseConfig(version=2).get_args(kb_name=args.kb_name, model=args.model_name, template=args.template)
 
     dataset = DataReader.load_json(config.entity_path)
 
-    test_dataset = WNDataset(data=dataset, dataset_type='test', template=args.template, is_train=False)
+    test_dataset = BaselineInferenceDatasetFactory(kb_name=args.kb_name, data=dataset, template=args.template)
+
     report_dict = {
         "baseline-run-args": str(args),
         "report_output_refrence": config.report_output,
         "results":[],
         "dataset-in-use": str(test_dataset),
         "configs": vars(config),
-        "dataset-stats": dataset['stats']
+        "dataset-stats": test_dataset.get_stats()
     }
     test_dataloader = DataLoader(test_dataset, batch_size=config.batch_size, shuffle=False, collate_fn=test_dataset.collate_fn)
 
