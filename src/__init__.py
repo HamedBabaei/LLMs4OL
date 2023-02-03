0597d26e6c21d38ac57340c7b4e03f3c57331fb8
Hamed Babaei <hamedbabaeigiglou@gmail.com>
2023-02-03 12:56:23 +0100

    [add] add packaging imports

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,5 @@
 from .utils import geoname_taxonomy_recursive
 from .entity_dataset_builder import EntityDatasetBuilderFactory
+from .evaluation import EvaluationFactory
+from .dataset import WNDataset
+from .baselines import BaselineFactory
