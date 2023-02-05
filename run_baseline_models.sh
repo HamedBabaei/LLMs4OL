b6972905f67932cb06dc297544f42f45b0e9cbf1
Hamed Babaei <hamedbabaeigiglou@gmail.com>
2023-02-05 20:03:51 +0100

    [update] baseline runner

--- run_baseline_models.sh
+++ run_baseline_models.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Enter dataset out of (wn18rr, geonames, nci):"
+echo "Enter dataset out of (wn18rr, geonames, nci, snomedct_us, medicin):"
 read kb_name
 
 echo "Enter output logs path for $kb_name results:"
@@ -13,6 +13,7 @@ exec > $log
 echo "Running baseline models!"
 
 templates=("template-1" "template-2" "template-3" "template-4" "template-5" "template-6" "template-7" "template-8")
+
 for template in "${templates[@]}"; do
   echo "Running $model_name for template: $template!"
   python3 run_baseline_models.py --kb_name=$kb_name --model_name=$model_name --template=$template
