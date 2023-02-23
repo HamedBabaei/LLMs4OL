c351d9b16d3e5f4fd3c4bac92ac721b9848bc849
Hamed Babaei <hamedbabaeigiglou@gmail.com>
2023-02-23 18:57:56 +0100

    [TaskA][update] prepare datasets

--- TaskA/prepare_datasets.py
+++ TaskA/prepare_datasets.py
@@ -14,9 +14,9 @@ def make_wn18rr(config):
         df["tail-def"] = df['tail'].map(name2defs_)
         df['head-entity-type'] = df['head'].apply(lambda X: X.split("_")[-2])
         df['tail-entity-type'] = df['tail'].apply(lambda X: X.split("_")[-2])
-        df = df.drop(df[df['relation'].isin(config.relations_to_ignore)].index)
-        df = df.drop(df[df['head-entity-type'].isin(config.entity_class_to_ignore)].index)
-        df = df.drop(df[df['tail-entity-type'].isin(config.entity_class_to_ignore)].index)
+        # df = df.drop(df[df['relation'].isin(config.relations_to_ignore)].index)
+        # df = df.drop(df[df['head-entity-type'].isin(config.entity_class_to_ignore)].index)
+        # df = df.drop(df[df['tail-entity-type'].isin(config.entity_class_to_ignore)].index)
         return df
 
     def make_entity_detection_dataset(df, count=-1, use_sampling=False):
@@ -51,9 +51,9 @@ def make_wn18rr(config):
     train, test, valid = make_replacements(train, id2name, name2defs), \
                          make_replacements(test, id2name, name2defs), \
                          make_replacements(valid, id2name, name2defs)
-    train_e, test_e, valid_e = make_entity_detection_dataset(train, count=10000, use_sampling=True), \
-                               make_entity_detection_dataset(test, count=3700, use_sampling=True), \
-                               make_entity_detection_dataset(valid, count=3700, use_sampling=True)
+    train_e, test_e, valid_e = make_entity_detection_dataset(train, count=-1, use_sampling=False), \
+                               make_entity_detection_dataset(test, count=-1, use_sampling=False), \
+                               make_entity_detection_dataset(valid, count=-1, use_sampling=False)
 
     new_train = train[train['head'].isin(train_e['entity'].tolist()) & train['tail'].isin(train_e['entity'].tolist())] 
     new_test = test[test['head'].isin(test_e['entity'].tolist()) & test['tail'].isin(test_e['entity'].tolist())] 
@@ -95,25 +95,25 @@ def make_geoname(config):
             dfs.append(df)            
         df = pd.concat(dfs)
         return df
-        
+
     feature_codes_df = DataReader.load_csv(config.feature_codes, sep='\t', 
                                            names=["feature-code", "name", "description"])
     feature_codes_df = feature_codes_df.drop(feature_codes_df[feature_codes_df['feature-code'].isnull()].index)
-    feature_codes_df['level-1'] = feature_codes_df['feature-code'].apply(lambda X: str(X)[0])
-    feature_codes_df['level-3'] = feature_codes_df['feature-code'].apply(lambda X: str(X)[2:])
-
-    level2_mapper = {}
-    for group, group_df in feature_codes_df.groupby("level-1"):
-        group_df_level2_taxonomy = geoname_taxonomy_recursive(group_df['level-3'].tolist(), depth=config.depth)
-        print(f"group {group}: number of depth-2 items:‌ {len(group_df_level2_taxonomy)}")
-        for depth2_name, depth2_items in group_df_level2_taxonomy.items():
-            for depth3_name, depth3_items in depth2_items.items():
-                    for depth3_item in depth3_items:
-                        level2_mapper[depth3_item] = depth3_name
-
-    feature_codes_df['level-2'] = feature_codes_df['level-3'].map(level2_mapper)
-    level3_columns = feature_codes_df.pop("level-3")
-    feature_codes_df.insert(5, "level-3", level3_columns)
+    # feature_codes_df['level-1'] = feature_codes_df['feature-code'].apply(lambda X: str(X)[0])
+    # feature_codes_df['level-3'] = feature_codes_df['feature-code'].apply(lambda X: str(X)[2:])
+    #
+    # level2_mapper = {}
+    # for group, group_df in feature_codes_df.groupby("level-1"):
+    #     group_df_level2_taxonomy = geoname_taxonomy_recursive(group_df['level-3'].tolist(), depth=config.depth)
+    #     print(f"group {group}: number of depth-2 items: {len(group_df_level2_taxonomy)}")
+    #     for depth2_name, depth2_items in group_df_level2_taxonomy.items():
+    #         for depth3_name, depth3_items in depth2_items.items():
+    #                 for depth3_item in depth3_items:
+    #                     level2_mapper[depth3_item] = depth3_name
+    #
+    # feature_codes_df['level-2'] = feature_codes_df['level-3'].map(level2_mapper)
+    # level3_columns = feature_codes_df.pop("level-3")
+    # feature_codes_df.insert(5, "level-3", level3_columns)
     
     
     all_countries_df = DataReader.load_csv(config.all_countries, sep='\t', low_memory=False, 
@@ -123,13 +123,15 @@ def make_geoname(config):
                                                  "elevation", "dem", "timezone", "modification-date"])[["name", "asciiname", "country-code",
                                                                                                         "feature-class", "feature-code"]]
     all_countries_df = all_countries_df.drop(all_countries_df[all_countries_df['feature-class'].isnull()].index)
-    all_countries_df.rename(columns={"feature-class":"level-1", "feature-code":"level-3"}, inplace=True)
-    all_countries_df['level-2'] = all_countries_df['level-3'].map(level2_mapper)
-    level3_columns = all_countries_df.pop("level-3")
-    all_countries_df.insert(5, "level-3", level3_columns)
+    all_countries_df = all_countries_df.drop(all_countries_df[all_countries_df['feature-code'].isnull()].index)
+    all_countries_df = all_countries_df.drop_duplicates()
+    all_countries_df.rename(columns={"feature-class":"level-1", "feature-code":"level-2"}, inplace=True)
+    # all_countries_df['level-2'] = all_countries_df['level-3'].map(level2_mapper)
+    # level3_columns = all_countries_df.pop("level-3")
+    # all_countries_df.insert(5, "level-3", level3_columns)
 
-    all_countries_df = make_level2_reduction(all_countries_df, max_class_no=10)
-    all_countries_df = make_level3_reduction(all_countries_df, count_no=1_000_000)
+    # all_countries_df = make_level2_reduction(all_countries_df, max_class_no=10)
+    # all_countries_df = make_level3_reduction(all_countries_df, count_no=1_000_000)
 
     print(f"Szie of dataset is:{all_countries_df.shape[0]:_}")
     print("Level 1 class FQs:\n", all_countries_df['level-1'].value_counts())
@@ -157,9 +159,9 @@ def make_umls(config):
 
     def filter_based_on_sab(umls_rel, umls_entity, sab, rel_threshold=1000):
         sab_rel_df = umls_rel[umls_rel['SAB-CUI1'].isin([sab]) & umls_rel['SAB-CUI2'].isin([sab])].reset_index()
-        sab_rel_df['RELA'] = sab_rel_df['RELA'].apply(cleaner)
-        sab_rel_df = sab_rel_df[sab_rel_df['RELA'].isin(list(sab_rel_df['RELA'].value_counts().loc[lambda X: X >= rel_threshold].keys()))].reset_index()
-        print(f"# of samples in {sab} relation dataset is : {sab_rel_df.shape[0]:_}")
+        # sab_rel_df['RELA'] = sab_rel_df['RELA'].apply(cleaner)
+        # sab_rel_df = sab_rel_df[sab_rel_df['RELA'].isin(list(sab_rel_df['RELA'].value_counts().loc[lambda X: X >= rel_threshold].keys()))].reset_index()
+        # print(f"# of samples in {sab} relation dataset is : {sab_rel_df.shape[0]:_}")
         sab_ents_list = sab_rel_df['CUI1'].tolist() + sab_rel_df['CUI2'].tolist()
         sab_ents_list = list(set(sab_ents_list))
         sab_ent_df = umls_entity[umls_entity['CUI'].isin(sab_ents_list)].reset_index()
@@ -173,18 +175,18 @@ def make_umls(config):
         sab_path = os.path.join(config.umls_processed_dir, sab)
         mkdir(sab_path)
         umls_rel_sab_df, umls_ent_sab_df = filter_based_on_sab(umls_rel_df, umls_entity_df, sab)
-        print("# of unique relations:", len(umls_rel_sab_df['RELA'].value_counts()))
-        DataWriter.write_df(umls_rel_sab_df, os.path.join(sab_path, "UMLS_" + sab + "_REL.csv"))
+        # print("# of unique relations:", len(umls_rel_sab_df['RELA'].value_counts()))
+        # DataWriter.write_df(umls_rel_sab_df, os.path.join(sab_path, "UMLS_" + sab + "_REL.csv"))
         DataWriter.write_df(umls_ent_sab_df, os.path.join(sab_path, "UMLS_" + sab + "_ENT.csv"))
         print("-"*40)
 
 if __name__ == "__main__":
-    # config = BaseConfig(version=2).get_args(kb_name="wn18rr")
-    # make_wn18rr(config=config)
+    config = BaseConfig(version=3).get_args(kb_name="wn18rr")
+    make_wn18rr(config=config)
 
-    # config = BaseConfig(version=2).get_args(kb_name="geonames")
-    # make_geoname(config=config)
+    config = BaseConfig(version=3).get_args(kb_name="geonames")
+    make_geoname(config=config)
 
-    config = BaseConfig(version=2).get_args(kb_name="umls")
+    config = BaseConfig(version=3).get_args(kb_name="umls")
     make_umls(config=config)
 
