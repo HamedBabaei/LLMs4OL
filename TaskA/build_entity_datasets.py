1951737badb88683d17a00ff1ac12ac203973f11
Hamed Babaei <hamedbabaeigiglou@gmail.com>
2023-02-24 16:19:16 +0100

    [TaskA][update]  task A dataset builder

--- TaskA/build_entity_datasets.py
+++ TaskA/build_entity_datasets.py
@@ -6,13 +6,13 @@ from datahandler import DataReader, DataWriter
 if __name__=="__main__":
     dataset_builder = EntityDatasetBuilderFactory(loader=DataReader)
 
-    # config = BaseConfig(version=3).get_args(kb_name="wn18rr")
-    # wn_builder = dataset_builder(config=config)
-    # dataset_json, dataset_stats = wn_builder.build()
-    # DataWriter.write_json(data=dataset_json,
-    #                       path=config.entity_path)
-    # DataWriter.write_json(data=dataset_stats,
-    #                       path=config.dataset_stats)
+    config = BaseConfig(version=3).get_args(kb_name="wn18rr")
+    wn_builder = dataset_builder(config=config)
+    dataset_json, dataset_stats = wn_builder.build()
+    DataWriter.write_json(data=dataset_json,
+                          path=config.entity_path)
+    DataWriter.write_json(data=dataset_stats,
+                          path=config.dataset_stats)
 
     config = BaseConfig(version=3).get_args(kb_name="geonames")
     geo_builder = dataset_builder(config=config)
@@ -22,9 +22,11 @@ if __name__=="__main__":
     DataWriter.write_json(data=dataset_stats,
                           path=config.dataset_stats)
 
-    # config = BaseConfig(version=3).get_args(kb_name="umls")
-    # umls_builder = dataset_builder(config=config)
-    # dataset_json = umls_builder.build()
-    # for kb in list(dataset_json.keys()):
-    #     DataWriter.write_json(data=dataset_json[kb],
-    #                         path=BaseConfig(version=2).get_args(kb_name=kb.lower()).entity_path)
+    config = BaseConfig(version=3).get_args(kb_name="umls")
+    umls_builder = dataset_builder(config=config)
+    dataset_json, dataset_stats = umls_builder.build()
+    for kb in list(dataset_json.keys()):
+        DataWriter.write_json(data=dataset_json[kb],
+                              path=BaseConfig(version=3).get_args(kb_name=kb.lower()).entity_path)
+        DataWriter.write_json(data=dataset_stats[kb],
+                              path=BaseConfig(version=3).get_args(kb_name=kb.lower()).dataset_stats)
