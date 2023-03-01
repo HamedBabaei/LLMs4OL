827b0970eac185d8a30da7e3884a2000b25a97fb
Hamed Babaei <hamedbabaeigiglou@gmail.com>
2023-03-01 18:25:02 +0100

    [add] new evaluation metric

--- TaskA/src/evaluation.py
+++ TaskA/src/evaluation.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 def precision_at_k(actual, predicted):
     act_set = set(actual)
@@ -6,67 +6,83 @@ def precision_at_k(actual, predicted):
     result = len(act_set & pred_set) / float(len(predicted))
     return result*100
 
-def recall_at_k(actual, predicted):
-    act_set = set(actual)
-    pred_set = set(predicted)
-    result = len(act_set & pred_set) / float(len(act_set))
-    return result*100
+def apk(actual, predicted, k):
+    """
+    Computes the average precision at k.
+    This function computes the average prescision at k between two lists of
+    items.
+    Parameters
+    ----------
+    actual : list
+             A list of elements that are to be predicted (order doesn't matter)
+    predicted : list
+                A list of predicted elements (order does matter)
+    k : int
+        The maximum number of predicted elements
+    Returns
+    -------
+    score : double
+            The average precision at k over the input lists
+    """
+    if not actual:
+        return 0.0
+    if len(predicted)>k:
+        predicted = predicted[:k]
+    score = 0.0
+    num_hits = 0.0
+    for i,p in enumerate(predicted):
+        # first condition checks whether it is valid prediction
+        # second condition checks if prediction is not repeated
+        if p in actual and p not in predicted[:i]:
+            num_hits += 1.0
+            score += num_hits / (i+1.0)
+    return score / min(len(actual), k)
 
 
-class Evaluation:
-
-    def __init__(self) -> None:
-        pass
-
-    def evaluate(self, actuals:list, predictions:list):
-        scores = []
-        for actual, prediction in zip(actuals, predictions):
-            scores.append(self._evaluate(actual=actual, prediction=prediction))
-        return self._results(scores)
-
-    def _evaluate(self, actual:list, prediction:list) -> list:
-        pass
-
-    def _results(self, scores) -> dict:
-        pass
-
-
-class MP(Evaluation):
-
-    def __init__(self) -> None:
-        super().__init__()
-
-    def _evaluate(self, actual: list, prediction: list) -> list:
-        precision_at_1 = precision_at_k(actual=actual, predicted=prediction[:1])
-        precision_at_5 = precision_at_k(actual=actual, predicted=prediction[:5])
-        precision_at_10 = precision_at_k(actual=actual, predicted=prediction[:10])
-        
-        recall_at_1 = recall_at_k(actual=actual, predicted=prediction[:1])
-        recall_at_5 = recall_at_k(actual=actual, predicted=prediction[:5])
-        recall_at_10 = recall_at_k(actual=actual, predicted=prediction[:10])
-
-        return [precision_at_1, precision_at_5, precision_at_10, recall_at_1, recall_at_5, recall_at_10]
-
-    def _results(self, scores: list) -> dict:
-        mp_at_1 = sum([score[0] for score in scores])/len(scores)
-        mp_at_5 = sum([score[1] for score in scores])/len(scores)
-        mp_at_10 = sum([score[2] for score in scores])/len(scores)
-        
-        mr_at_1 = sum([score[3] for score in scores])/len(scores)
-        mr_at_5 = sum([score[4] for score in scores])/len(scores)
-        mr_at_10 = sum([score[5] for score in scores])/len(scores)
-
-        return {"P@1": mp_at_1, "P@5": mp_at_5, "P@10": mp_at_10, 
-                "R@1": mr_at_1, "R@5": mr_at_5, "R@10": mr_at_10}
+def mapk(actual, predicted, k):
+    """
+    Computes the mean average precision at k.
+    This function computes the mean average prescision at k between two lists
+    of lists of items.
+    Parameters
+    ----------
+    actual : list
+             A list of lists of elements that are to be predicted
+             (order doesn't matter in the lists)
+    predicted : list
+                A list of lists of predicted elements
+                (order matters in the lists)
+    k : int,
+        The maximum number of predicted elements
+    Returns
+    -------
+    score : double
+            The mean average precision at k over the input lists
+    """
+    return np.mean([apk(a, p, k) for a,p in zip(actual, predicted)])
 
 
-class EvaluationFactory:
+class EvaluationMetrics:
 
-    def __init__(self) -> None:
-        self.metrics = {
-            "mp":MP
-        }
+    def __init__(self, ks:list, metric="map") -> None:
+        self.ks = ks
+        self.metric=metric
 
-    def __call__(self, metric_name):
-        return self.metrics[metric_name]()
-        
+    def evaluate(self, actual:list, predicted:list):
+        if self.metric == "map":
+            return self.MAP(actual, predicted)
+        else:
+            return self.AP(actual, predicted)
+
+    def MAP(self, actual:list, predicted:list):
+        results_dict = {}
+        for k in self.ks:
+            results_dict["MAP@"+str(k)] = mapk(actual=actual, predicted=predicted, k=k)
+        return results_dict
+
+    def AP(self, actual:list, predicted:list):
+        results_dict = {}
+        for k in self.ks:
+            results_dict["AP@"+str(k)] = [apk(actual=actual, predicted=predicted, k=k)
+                                          for a, p in zip(actual, predicted)]
+        return results_dict
