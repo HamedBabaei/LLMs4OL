b9d9da51b6efd2720c3c022961a0ceeee7ab92eb
Hamed Babaei <hamedbabaeigiglou@gmail.com>
2023-03-01 18:23:32 +0100

    [add] device option to sh and fix codes

--- TaskA/test_auto.sh
+++ TaskA/test_auto.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 label="nofinetuning"
+device="cpu"
 datasets=("wn18rr" "geonames" "nci" "snomedct_us" "medcin")
 templates=("template-1" "template-2" "template-3" "template-4" "template-5" "template-6" "template-7" "template-8")
-models=("bert_large" "flan_t5_large" "flan_t5_xl")
+models=("bert_large" "flan_t5_large" "flan_t5_xl" "bart_large")
 
 for kb_name in "${datasets[@]}"; do
   index=1
@@ -11,8 +12,8 @@ for kb_name in "${datasets[@]}"; do
     log="results/$kb_name/$index-$kb_name-$model_name.$label.test.log.txt"
     exec > $log
     for template in "${templates[@]}"; do
-      echo "Running $model_name for template: $template!"
-      echo "$kb_name-$model_name-$template.log"
+      echo "Running on dataset: $kb_name , model: $model_name, template: $template!"
+      python3 test.py --kb_name=$kb_name --model_name=$model_name --template=$template --device=$device
       echo "Inference for  $model_name on template: $template  is done"
       echo "-----------------------------------------------------------"
     done
--- TaskA/test_manual.sh
+++ TaskA/test_manual.sh
@@ -3,7 +3,9 @@ read -p "Enter dataset out of (wn18rr, geonames, nci, snomedct_us, medcin):" kb_
 
 read -p "Enter output logs path for $kb_name results:" log
 
-read -p "Enter the name of model out of (bert_large, flan_t5_large, flan_t5_xl):" model_name
+read -p "Enter the name of model out of (bert_large, flan_t5_large, flan_t5_xl, bart_large):" model_name
+
+read -p "Enter the device (cpu, cuda, cuda:1, cuda:2, ...):" device
 
 exec > $log
 
@@ -13,7 +15,7 @@ templates=("template-1" "template-2" "template-3" "template-4" "template-5" "tem
 
 for template in "${templates[@]}"; do
   echo "Running $model_name for template: $template!"
-  python3 level1_inference_runner.py --kb_name=$kb_name --model_name=$model_name --template=$template
+  python3 test.py --kb_name=$kb_name --model_name=$model_name --template=$template --device=$device
   echo "Inference for  $model_name on template: $template  is done"
   echo "-----------------------------------------------------------"
 done
