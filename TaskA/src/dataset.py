f901895e28bf9913c0cb322677a4d6c108f5deca
Hamed Babaei <hamedbabaeigiglou@gmail.com>
2023-03-06 19:04:23 +0100

    [TaskA][update] geonames and umls datasets

--- TaskA/src/dataset.py
+++ TaskA/src/dataset.py
@@ -2,14 +2,12 @@
 from torch.utils.data import Dataset
 
 class WNDataset(Dataset):
-    def __init__(self, data, kb_name, templates, template, dataset_type, is_train, label_in_list=True):
+    def __init__(self, data, templates, template, is_train):
         self.data = data
         self.template = templates[template]
         self.is_train = is_train
-        self.dataset_type = dataset_type
+        self.dataset_type = "train" if self.is_train else "test"
         self.use_sentence =  True if "[SENTENCE]" in self.template else False
-        self.label_in_list = label_in_list
-
         print(f"WNDataset:{'Train-SET' if self.is_train else 'Test-SET'} --- {template}: {self.template}")
     
     def __len__(self):
@@ -21,8 +19,7 @@ class WNDataset(Dataset):
     def __getitem__(self, index):
         item = self.data[self.dataset_type][index]
         sample, label = self.template.replace("[A]", item['entity']), item['label']
-        if self.label_in_list:
-            label = [label]
+        label = [label]
         if self.use_sentence:
             sample = sample.replace("[SENTENCE]", item['sentence'])
         if self.is_train:
@@ -37,42 +34,36 @@ class WNDataset(Dataset):
         return batchs_clear
 
 class GeonameDataset(Dataset):
-    def __init__(self, data, kb_name, templates, template, dataset_type, is_train, label_in_list=True):
+    def __init__(self, data, kb_name, templates, template, is_train, label_mapper):
         self.template = templates[template]
         self.is_train = is_train
-        self.level_key = dataset_type
-        self.stats = data['stats']
-        self.use_country =  True if "[COUNTRY]" in self.template else False
-        self.label_in_list = label_in_list
+        self.label_mapper = label_mapper
+        self.use_country = True if "[COUNTRY]" in self.template else False
         self.data = []
-        level_search_key = "status-in-"+self.level_key
         for sample in data['geonames']:
-            if level_search_key in list(sample.keys()):
-                if self.is_train and sample[level_search_key] == "train":
-                    self.data.append({
-                        "asciname": sample['asciname'],
-                        'label-strings-in-'+self.level_key: sample['label-strings-in-'+self.level_key],
-                        "country_name": sample['country_name']
-                    })
-                if not self.is_train and sample[level_search_key] == "test":
-                    self.data.append(sample)
-        print(f"GeonamesDataset:{'Train-SET' if self.is_train else 'Test-SET'}-{self.level_key} --- {template}: {self.template}")
+            if self.is_train and sample['status'] == "train":
+                self.data.append(sample)
+            if not self.is_train and sample['status'] == "test":
+                self.data.append(sample)
+        # self.data = self.data[:100]
+        print(f"GeonamesDataset:{'Train-SET' if self.is_train else 'Test-SET'} --- {template}: {self.template}")
     
     def __len__(self):
         return len(self.data)
     
     def __repr__(self) -> str:
-         return f"GeonamesDataset:{'Train-SET' if self.is_train else 'Test-SET'}-{self.level_key} --- Template-in-use: {self.template}"
+         return f"GeonamesDataset:{'Train-SET' if self.is_train else 'Test-SET'} --- Template-in-use: {self.template}"
     
     def __getitem__(self, index):
         item = self.data[index]
-        sample, label = self.template.replace("[A]", str(item['asciname'])), item['label-strings-in-'+self.level_key]
-        if self.label_in_list:
-            label = label
+        sample, label = self.template.replace("[A]", str(item['asciname'])), item['type-label']
+        labels = self.label_mapper[label[0]]+[self.label_mapper[label]['name']]+self.label_mapper[label]['synonyms']
+        label = list(set(labels))
+        label = [l.lower() for l in label]
         if self.use_country:
-            sample = sample.replace("[COUNTRY]", str(item['country_name']))
+            sample = sample.replace("[COUNTRY]", str(item["country_name"]))
         if self.is_train:
-            sample = sample.replace("[MASK]", 'or '.join(item['label-strings-in-'+self.level_key]))
+            sample = sample.replace("[MASK]", 'or '.join(item['type-name']))
         return {"sample":sample, "label":label}
 
     def collate_fn(self, batchs):
@@ -83,39 +74,39 @@ class GeonameDataset(Dataset):
         return batchs_clear
 
 class UMLSDataset(Dataset):
-    def __init__(self, data, kb_name, templates, template, dataset_type, is_train, label_in_list=True):
+    def __init__(self, data, kb_name, templates, template, is_train, label_mapper):
         self.template = templates[template]
         self.is_train = is_train
-        self.level_key = dataset_type
-        self.stats = data['stats']
         self.kb_name = kb_name
+        self.label_mapper = label_mapper
         self.use_sentence =  True if "[SENTENCE]" in self.template else False
-        self.label_in_list = label_in_list
         self.data = []
-        level_search_key = "status-in-"+self.level_key
         for sample in data['umls']:
-            if level_search_key in list(sample.keys()):
-                if self.is_train and sample[level_search_key] == "train":
-                    self.data.append(sample)
-                if not self.is_train and sample[level_search_key] == "test":
-                    self.data.append(sample)
-        print(f"UMLSDataset:{'Train-SET' if self.is_train else 'Test-SET'}-{self.kb_name}-{self.level_key} --- {template}: {self.template}")
+            if self.is_train and sample["status"] == "train":
+                self.data.append(sample)
+            if not self.is_train and sample["status"] == "test":
+                self.data.append(sample)
+        print(f"UMLSDataset:{'Train-SET' if self.is_train else 'Test-SET'}-{self.kb_name} --- {template}: {self.template}")
     
     def __len__(self):
         return len(self.data)
     
     def __repr__(self) -> str:
-         return f"UMLSDataset:{'Train-SET' if self.is_train else 'Test-SET'}-{self.kb_name}-{self.level_key} --- Template-in-use: {self.template}"
+         return f"UMLSDataset:{'Train-SET' if self.is_train else 'Test-SET'}-{self.kb_name} --- Template-in-use: {self.template}"
     
     def __getitem__(self, index):
         item = self.data[index]
-        sample, label = self.template.replace("[A]", str(item['concept'])), item['label-strings-in-'+self.level_key]
-        if self.label_in_list:
-            label = label
+        sample = self.template.replace("[A]", str(item['concept']))
+        labels = item['label-names']
+        for label in eval(item['label-str']):
+            for l in self.label_mapper[label]:
+                labels.append(l)
+        label = list(set(labels))
+        label = [l.lower() for l in label]
         if self.use_sentence:
-            sample = sample.replace("[SENTENCE]", str(item['sentence']))
+            sample = sample.replace("[SENTENCE]", str(item['concept']))
         if self.is_train:
-            sample = sample.replace("[MASK]", 'or '.join(item['label-strings-in-'+self.level_key]))
+            sample = sample # .replace("[MASK]", 'or '.join(item['label-names']))
         return {"sample":sample, "label":label}
 
     def collate_fn(self, batchs):
@@ -127,21 +118,17 @@ class UMLSDataset(Dataset):
 
 
 class InferenceDatasetFactory:
-    def __new__(CLS, kb_name, data, templates, template) -> Dataset:
+    def __new__(CLS, kb_name, data, templates, template, label_mapper) -> Dataset:
         if kb_name == "geonames":
             return GeonameDataset(data=data, kb_name=kb_name, 
-                                  templates=templates, template=template, 
-                                  dataset_type="level-1", is_train=False, 
-                                  label_in_list=True)
+                                  templates=templates, template=template,
+                                  is_train=False, label_mapper=label_mapper)
 
         if kb_name == "wn18rr":
-            return WNDataset(data=data, kb_name=kb_name, 
-                             templates=templates, template=template, 
-                             dataset_type="test", is_train=False, 
-                             label_in_list=True)
+            return WNDataset(data=data, templates=templates,
+                             template=template, is_train=False)
 
         if kb_name == "nci" or kb_name == "snomedct_us" or kb_name == "medicin":
             return UMLSDataset(data=data, kb_name=kb_name, 
-                                templates=templates, template=template, 
-                                dataset_type="level-1", is_train=False, 
-                                label_in_list=True)
+                                templates=templates, template=template,
+                                is_train=False)
