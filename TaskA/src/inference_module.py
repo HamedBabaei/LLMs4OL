dbbf5b58e92e6b383ff22cfeb3ca222582fc4998
Hamed Babaei <hamedbabaeigiglou@gmail.com>
2023-03-11 11:03:51 +0100

    [TaskA][fix] fix multi gpu tokenizers

--- TaskA/src/inference_module.py
+++ TaskA/src/inference_module.py
@@ -4,7 +4,6 @@ from transformers import T5Tokenizer, T5ForConditionalGeneration
 import torch
 import openai
 import time
-from parallelformers import parallelize
 
 
 class BaseLM:
@@ -12,14 +11,14 @@ class BaseLM:
     def __init__(self, config) -> None:
         self.config = config
         pass
-    
+
     def fit(self, X, Y):
         pass
 
-    def make_batch_prediction(self, Xs:list):
+    def make_batch_prediction(self, Xs: list):
         pass
 
-    def predict(self, X:str):
+    def predict(self, X: str):
         pass
 
 
@@ -40,8 +39,8 @@ class MaskedLM(BaseLM):
         self.device = self.config.device
         self.model.to(self.device)
         self.top_n = self.config.top_n
-        
-    def predict(self, X:str):        
+
+    def predict(self, X:str):
         inputs = self.tokenizer(X, return_tensors="pt")
         inputs.to(self.device)
         with torch.no_grad():
@@ -103,16 +102,18 @@ class EncoderDecoderLM(BaseLM):
             print(f"Loaded T5ForConditionalGeneration from {self.config.model_path}")
         self.device = self.config.device
         self.model.to(self.device)
+        self.model.eval()
         self.top_n = self.config.top_n
 
     def __output_cleaner(self, pred):
-        return pred.replace("<pad>", "").replace("</s>","").strip()
+        return pred.replace("<pad>", "").replace("</s>", "").strip()
 
-    def predict(self, X:str):
+    def predict(self, X: str):
         inputs = self.tokenizer(X, return_tensors="pt")
         inputs.to(self.device)
         with torch.no_grad():
-            sequence_ids = self.model.generate(inputs.input_ids, num_beams=200, num_return_sequences=self.top_n, max_length=5)
+            sequence_ids = self.model.generate(inputs.input_ids, num_beams=50, num_return_sequences=self.top_n,
+                                               max_length=5)
         sequences = self.tokenizer.batch_decode(sequence_ids, skip_special_tokens=True)
         sequences = [self.__output_cleaner(seq) for seq in sequences]
         logits = [0 for seq in sequences]
@@ -120,11 +121,14 @@ class EncoderDecoderLM(BaseLM):
 
     def make_batch_prediction(self, Xs):
         predictions, logits = [], []
-        inputs = self.tokenizer(Xs, return_tensors="pt", padding=True)
+        # inputs = self.tokenizer(Xs, return_tensors="pt", truncation=True, padding='max_length', max_length=256)
+        inputs = self.tokenizer(Xs, return_tensors="pt",padding=True)
         inputs.to(self.device)
         with torch.no_grad():
-            sequence_ids = self.model.generate(inputs.input_ids, num_beams=200, num_return_sequences=self.top_n, max_length=5)
-        sequences = self.tokenizer.batch_decode(sequence_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)
+            sequence_ids = self.model.generate(input_ids=inputs["input_ids"], attention_mask=inputs["attention_mask"],
+                                               num_beams=50, num_return_sequences=self.top_n, max_length=5)
+        sequences = self.tokenizer.batch_decode(sequence_ids.cpu(), skip_special_tokens=True,
+                                                clean_up_tokenization_spaces=False)
         sequences = [self.__output_cleaner(seq) for seq in sequences]
         sequences_logist = [0 for _ in sequences]
         for index in range(0, len(Xs)):
@@ -132,7 +136,7 @@ class EncoderDecoderLM(BaseLM):
             logits.append(sequences_logist[self.top_n * index:self.top_n * (index + 1)])
         return predictions, logits
 
-    def make_batch_prediction1(self, Xs):
+    def make_single_batch_prediction(self, Xs):
         predictions, logits = [], []
         for X in Xs:
             predict, logit = self.predict(X)
@@ -146,29 +150,22 @@ class EncoderDecoderLMMultiGPU(BaseLM):
     def __init__(self, config) -> None:
         super().__init__(config)
         self.model_name = config.template_name
-        if self.model_name == "bart":
-            self.tokenizer = AutoTokenizer.from_pretrained(self.config.model_path)
-            self.model = BartForConditionalGeneration.from_pretrained(self.config.model_path,
-                                                                      forced_bos_token_id=0)
-            print(f"Loaded BartForConditionalGeneration from{self.config.model_path}")
-        else:
-            self.tokenizer = T5Tokenizer.from_pretrained(self.config.model_path)
-            self.model = T5ForConditionalGeneration.from_pretrained(self.config.model_path)
-            print(f"Loaded T5ForConditionalGeneration from {self.config.model_path}")
-        parallelize(self.model, num_gpus=config.gpu_no, fp16=True, verbose='detail')
-        # self.device = self.config.device
-        # self.model.to(self.device)
-        self.model.cuda()
+        self.tokenizer = T5Tokenizer.from_pretrained(self.config.model_path)
+        self.model = T5ForConditionalGeneration.from_pretrained(self.config.model_path, device_map="balanced")
+        print(f"Loaded T5ForConditionalGeneration from {self.config.model_path}")
+        self.model.eval()
+        self.device = config.device
         self.top_n = self.config.top_n
 
     def __output_cleaner(self, pred):
-        return pred.replace("<pad>", "").replace("</s>","").strip()
+        return pred.replace("<pad>", "").replace("</s>", "").strip()
 
-    def predict(self, X:str):
+    def predict(self, X: str):
         inputs = self.tokenizer(X, return_tensors="pt")
-        # inputs.to(self.device)
+        inputs.to(self.device)
         with torch.no_grad():
-            sequence_ids = self.model.generate(**inputs, num_beams=200, num_return_sequences=self.top_n, max_length=5)
+            sequence_ids = self.model.generate(inputs.input_ids, num_beams=200, num_return_sequences=self.top_n,
+                                               max_length=5)
         sequences = self.tokenizer.batch_decode(sequence_ids, skip_special_tokens=True)
         sequences = [self.__output_cleaner(seq) for seq in sequences]
         logits = [0 for seq in sequences]
@@ -176,11 +173,14 @@ class EncoderDecoderLMMultiGPU(BaseLM):
 
     def make_batch_prediction(self, Xs):
         predictions, logits = [], []
-        inputs = self.tokenizer(Xs, return_tensors="pt", padding=True)
-        # inputs.to(self.device)
+        inputs = self.tokenizer(Xs, return_tensors="pt", truncation=True, padding='max_length', max_length=256)
+        # inputs = self.tokenizer(Xs, return_tensors="pt", padding=True)
+        inputs.to(self.device)
         with torch.no_grad():
-            sequence_ids = self.model.generate(**inputs, num_beams=200, num_return_sequences=self.top_n, max_length=5)
-        sequences = self.tokenizer.batch_decode(sequence_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)
+            sequence_ids = self.model.generate(inputs.input_ids, max_length=5)
+            # sequence_ids = self.model.generate(inputs.input_ids, num_beams=50, num_return_sequences=self.top_n, max_length=5)
+        sequences = self.tokenizer.batch_decode(sequence_ids, skip_special_tokens=True,
+                                                clean_up_tokenization_spaces=False)
         sequences = [self.__output_cleaner(seq) for seq in sequences]
         sequences_logist = [0 for _ in sequences]
         for index in range(0, len(Xs)):
@@ -195,20 +195,19 @@ class Left2RightOnlineLM(BaseLM):
         self.model_name = config.template_name
         openai.api_key = config.openai_key
 
-
     def __output_cleaner(self, pred):
         return pred.rstrip('\n').strip()
 
-    def predict(self, X:str):
+    def predict(self, X: str):
         response = openai.Completion.create(
-                  model=self.config.model_path,
-                  prompt=X,
-                  temperature=0.7,
-                  max_tokens=10,
-                  top_p=1,
-                  frequency_penalty=0,
-                  presence_penalty=0
-                )
+            model=self.config.model_path,
+            prompt=X,
+            temperature=0.7,
+            max_tokens=10,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
         sequences = [self.__output_cleaner(response.choices[0].text)]
         logits = [0]
 
@@ -241,4 +240,4 @@ class InferenceFactory:
 
     def __call__(self, model_name):
         return self.models[model_name](config=self.config)
-        
+
