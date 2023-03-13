f98b560bf62da4aac7a8825ecc8bb707b7465fbb
Hamed Babaei <hamedbabaeigiglou@gmail.com>
2023-03-13 15:44:05 +0100

    [TaskB][update] configs

--- TaskB/configuration/config.py
+++ TaskB/configuration/config.py
@@ -15,20 +15,22 @@ class BaseConfig:
         self.parser = argparse.ArgumentParser()
         self.argument_getter = {
             "geonames": ["Geonames", self.add_geoname],
-            "nci": ["NCI", self.add_umls],
-            "snomedct_us": ["SNOMEDCT_US", self.add_umls],
-            "medcin": ["MEDCIN", self.add_umls]
+            "umls":["UMLS", self.add_umls]
         }
+        self.root_dir = "../datasets/TaskB"
 
     def add_geoname(self, dataset: str):
-        self.parser.add_argument("--raw_feature_codes", type=str, default=f"dataset/{dataset}/raw/featureCodes_en.txt")
-        self.parser.add_argument("--feature_codes", type=str, default=f"dataset/{dataset}/processed/feature_codes.csv")
+        self.parser.add_argument("--raw_feature_codes", type=str, default=f"{self.root_dir}/{dataset}/raw/featureCodes_en.txt")
+        self.parser.add_argument("--feature_codes", type=str, default=f"{self.root_dir}/{dataset}/processed/feature_codes.csv")
+        self.parser.add_argument("--processed_hier", type=str, default=f"{self.root_dir}/{dataset}/processed/hierarchy_dict.json")
 
     def add_umls(self, dataset: str):
-        self.parser.add_argument("--raw_sn", type=str,
-                                 default=f"dataset/{dataset}/raw/semantic_network.json")
-        self.parser.add_argument("--processed_sn", type=str,
-                                 default=f"dataset/{dataset}/processed/semantic_network.csv")
+        self.parser.add_argument("--raw_sn_nci", type=str, default=f"{self.root_dir}/{dataset}/raw/nci_semantic_network.json")
+        self.parser.add_argument("--raw_sn_medcin", type=str, default=f"{self.root_dir}/{dataset}/raw/medcin_semantic_network.json")
+        self.parser.add_argument("--raw_sn_snomedct_us", type=str, default=f"{self.root_dir}/{dataset}/raw/snomedct_us_semantic_network.json")
+        self.parser.add_argument("--raw_sn", type=str, default=f"{self.root_dir}/{dataset}/raw/semantic_network.json")
+        self.parser.add_argument("--processed_sn", type=str, default=f"{self.root_dir}/{dataset}/processed/semantic_network.csv")
+        self.parser.add_argument("--processed_hier", type=str, default=f"{self.root_dir}/{dataset}/processed/hierarchy_dict.json")
 
     def get_args(self, kb_name:str):
         """
