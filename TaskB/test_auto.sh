c37a3537969f513a4ae6c787d89d45cb306aab4e
Hamed Babaei <hamedbabaeigiglou@gmail.com>
2023-03-17 16:23:37 +0100

    [TaskA][add] test shell scripts

---
+++ TaskB/test_auto.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+label="nofinetuning"
+device="cpu"
+datasets=("geonames" "umls" "schema")
+templates=("0" "1" "2" "3" "4" "5" "6" "7")
+models=("bert_large" "flan_t5_large" "flan_t5_xl" "bart_large")
+
+for kb_name in "${datasets[@]}"; do
+  index=1
+  for model_name in  "${models[@]}"; do
+    log="results/$kb_name/$index-$kb_name-$model_name.$label.test.log.txt"
+    exec > $log
+    for template in "${templates[@]}"; do
+      echo "Running on dataset: $kb_name , model: $model_name, template: $template!"
+      python3 test.py --kb_name=$kb_name --model=$model_name --template=$template --device=$device
+      echo "Inference for  $model_name on template: $template  is done"
+      echo "-----------------------------------------------------------"
+    done
+    index=$((index+1))
+  done
+done
---
+++ TaskB/test_manual.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+read -p "Enter dataset out of (geonames, umls, schema):" kb_name
+
+read -p "Enter output logs path for $kb_name results:" log
+
+read -p "Enter the name of model out of (bert_large, flan_t5_large, flan_t5_xl, bart_large):" model_name
+
+read -p "Enter the device (cpu, cuda, cuda:1, cuda:2, ...):" device
+
+exec > $log
+
+echo "Running baseline models!"
+
+templates=("1" "2" "3" "4" "5" "6" "7" "8")
+
+for template in "${templates[@]}"; do
+  echo "Running $model_name for template: $template!"
+  python3 test.py --kb_name=$kb_name --model=$model_name --template=$template --device=$device
+  echo "Inference for  $model_name on template: $template  is done"
+  echo "-----------------------------------------------------------"
+done
