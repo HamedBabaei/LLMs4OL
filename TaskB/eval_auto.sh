6d280a98106b6e1557c1f050cad5811ce97cb31b
Hamed Babaei <hamedbabaeigiglou@gmail.com>
2023-04-11 19:17:09 +0200

    [add] add task B eval auto shell script

---
+++ TaskB/eval_auto.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+label="nofinetuning"
+
+datasets=("geonames" "schema" "umls")
+templates=("-0-" "-1-" "-2-" "-3-" "-4-" "-5-" "-6-" "-7-")
+models=("gpt3" "gpt3_ada")
+
+for kb_name in "${datasets[@]}"; do
+  index=8
+  for model_name in  "${models[@]}"; do
+    log="results/$kb_name/$index-$kb_name-$model_name.$label.test.log.txt"
+    exec > $log
+    for template in "${templates[@]}"; do
+      echo "Running on dataset: $kb_name , model: $model_name, template: $template!"
+      python3 evaluator.py --kb_name=$kb_name --model=$model_name --template=$template
+      echo "Inference for  $model_name on template: $template  is done"
+      echo "-----------------------------------------------------------"
+    done
+    index=$((index+1))
+  done
+done
