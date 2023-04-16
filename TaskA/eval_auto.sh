64ce2f9e3e4ab5e54da673f4e75b5961e16e127a
Hamed Babaei <hamedbabaeigiglou@gmail.com>
2023-04-17 01:36:19 +0200

    [update] task a evaluator shell script

--- TaskA/eval_auto.sh
+++ TaskA/eval_auto.sh
@@ -4,10 +4,10 @@ label="nofinetuning"
 
 datasets=("wn18rr" "geonames" "nci" "snomedct_us" "medcin")
 templates=("template-1" "template-2" "template-3" "template-4" "template-5" "template-6" "template-7" "template-8")
-models=("gpt3")
+models=("gpt3" "bloom_1b7" "bloom_3b")
 
 for kb_name in "${datasets[@]}"; do
-  index=5
+  index=9
   for model_name in  "${models[@]}"; do
     log="results/$kb_name/$index-$kb_name-$model_name.$label.test.log.txt"
     exec > $log
