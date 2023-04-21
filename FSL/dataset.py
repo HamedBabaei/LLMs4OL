bfa2353539ab99b8a32c32517798e4e0960b3220
Hamed Babaei <hamedbabaeigiglou@gmail.com>
2023-04-21 18:22:37 +0200

    [Add] add FSL dataset module

--- FSL/dataset.py
+++ FSL/dataset.py
@@ -1,24 +1,58 @@
+import templates as TEMPLATES
 
 class BaseDataset:
-    pass
+    def __int__(self, templates):
+        self.templates_list = templates
+    def build_samples(self, dataset):
+        source_text, target_text = [], []
+        for data in dataset:
+            for template in self.templates_list:
+                if "[LABEL]" in template and data['negative']:
+                    filled_template = self.fill_template(data, template)
+                    source_text.append(filled_template['text'])
+                    target_text.append(filled_template['label'])
+                if not data['negative']:
+                    filled_template = self.fill_template(data, template)
+                    source_text.append(filled_template['text'])
+                    target_text.append(filled_template['label'])
+        return source_text, target_text
 
 class WN(BaseDataset):
-    pass
-
-class GeoNames(BaseDataset):
-    pass
+    def fill_template(self, data, template):
+        template = template.replace("[A]", data['[A]'])
+        template = template.replace("[SENTENCE]", data['[SENTENCE]'])
+        if '[LABEL]' in template:
+            template = template.replace("[LABEL]", data['[LABEL]'])
+            return {"text": template, "label": data['yes-no-label']}
+        return {"text": template, "label": data['[LABEL]']}
 
 class UMLS(BaseDataset):
-    pass
+    def fill_template(self, data, template):
+        template = template.replace("[CONCEPT]", data['[CONCEPT]'])
+        template = template.replace("[SENTENCE]", data['[SENTENCE]'])
+        if '[LABEL]' in template:
+            template = template.replace("[LABEL]", data['[LABEL]'][0].lower())
+            return {"text": template, "label": data['yes-no-label']}
+        return {"text": template, "label": data['[LABEL]'][0].lower()}
+
+class GeoNames(BaseDataset):
+    def fill_template(self, data, template):
+        template = template.replace("[NAME]", data['[NAME]'])
+        template = template.replace("[COUNTRY]", data['[COUNTRY]'])
+        if '[LABEL]' in template:
+            template = template.replace("[LABEL]", data['[LABEL]'].lower())
+            return {"text": template, "label": data['yes-no-label']}
+        return {"text": template, "label": data['[LABEL]'].lower()}
+
 
 class DatasetFactory:
 
     def __new__(self, dataset):
         datasets = {
-            "wn": WN,
-            "GeoNames": GeoNames,
-            "UMLS": UMLS
+            "wn18rr": [WN, TEMPLATES.WN18RR],
+            "geonames": [GeoNames, TEMPLATES.GEONAMES],
+            "umls": [UMLS, TEMPLATES.UMLS]
         }
-        return datasets[dataset]
+        return datasets[dataset][0](templates=datasets[dataset][1])
 
 
