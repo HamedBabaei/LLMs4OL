01a18f62a2d50117abefde2e01078d3c1e570af5
Hamed Babaei <hamedbabaeigiglou@gmail.com>
2023-04-24 03:03:36 +0200

    update dataset.py with new templates

--- FSL/dataset.py
+++ FSL/dataset.py
@@ -7,44 +7,79 @@ class BaseDataset:
     def build_samples(self, dataset):
         source_text, target_text = [], []
         for data in dataset:
-            for template in self.templates_list:
-                if "[LABEL]" in template and data['negative']:
-                    filled_template = self.fill_template(data, template)
+            if data['negative']:
+                continue
+            if data.get('task-b', False):
+                for template in self.templates_list['task-b-prompts']:
+                    filled_template = self.fill_task_b_template(data, template)
                     source_text.append(filled_template['text'])
                     target_text.append(filled_template['label'])
-                if not data['negative']:
-                    filled_template = self.fill_template(data, template)
+            if data.get('task-c', False):
+                for template in self.templates_list['task-c-prompts']:
+                    filled_template = self.fill_task_c_template(data, template)
                     source_text.append(filled_template['text'])
                     target_text.append(filled_template['label'])
+            for template in self.templates_list['completion-prompts']:
+                filled_template = self.fill_completion_template(data, template)
+                source_text.append(filled_template['text'])
+                target_text.append(filled_template['label'])
         return source_text, target_text
 
+    def fill_completion_template(self, data, template) -> dict:
+        pass
+
+    def fill_task_b_template(self, data, template) -> dict:
+        pass
+
+    def fill_task_c_template(self, data, template) -> dict:
+        pass
+
+
 class WN(BaseDataset):
-    def fill_template(self, data, template):
+    def fill_completion_template(self, data, template) -> dict:
         template = template.replace("[A]", data['[A]'])
         template = template.replace("[SENTENCE]", data['[SENTENCE]'])
-        if '[LABEL]' in template:
-            template = template.replace("[LABEL]", data['[LABEL]'])
-            return {"text": template, "label": data['yes-no-label']}
         return {"text": template, "label": data['[LABEL]']}
 
 class UMLS(BaseDataset):
-    def fill_template(self, data, template):
+    def fill_completion_template(self, data, template) -> dict:
         template = template.replace("[CONCEPT]", data['[CONCEPT]'])
         template = template.replace("[SENTENCE]", data['[SENTENCE]'])
-        if '[LABEL]' in template:
-            template = template.replace("[LABEL]", data['[LABEL]'][0].lower())
-            return {"text": template, "label": data['yes-no-label']}
         return {"text": template, "label": data['[LABEL]'][0].lower()}
 
+    def fill_task_b_template(self, data, template) -> dict:
+        template = template.replace("[CONCEPT]", data['[CONCEPT]'])
+        template = template.replace("[SENTENCE]", data['[SENTENCE]'])
+        template = template.replace("[LABEL]", data['[LABEL]'][0].lower())
+        template = template.replace("[TEXT_A]", data['task-b-items']['text_a'].lower())
+        template = template.replace("[TEXT_B]", data['task-b-items']['text_b'].lower())
+        label = "yes" if data['task-b-items']['label'] == "correct" else "no"
+        return {"text": template, "label": label}
+
+    def fill_task_c_template(self, data, template) -> dict:
+        template = template.replace("[CONCEPT]", data['[CONCEPT]'])
+        template = template.replace("[SENTENCE]", data['[SENTENCE]'])
+        template = template.replace("[LABEL]", data['[LABEL]'][0].lower())
+        template = template.replace("[TAIL]", data['task-c-items']['t'].lower())
+        template = template.replace("[HEAD]", data['task-c-items']['h'].lower())
+        template = template.replace("[REL]", " ".join(data['task-c-items']['r'].lower().split("_")))
+        label = "yes" if data['task-c-items']['label'] == "correct" else "no"
+        return {"text": template, "label": label}
+
 class GeoNames(BaseDataset):
-    def fill_template(self, data, template):
+    def fill_completion_template(self, data, template) -> dict:
         template = template.replace("[NAME]", data['[NAME]'])
         template = template.replace("[COUNTRY]", data['[COUNTRY]'])
-        if '[LABEL]' in template:
-            template = template.replace("[LABEL]", data['[LABEL]'].lower())
-            return {"text": template, "label": data['yes-no-label']}
         return {"text": template, "label": data['[LABEL]'].lower()}
 
+    def fill_task_b_template(self, data, template) -> dict:
+        template = template.replace("[NAME]", data['[NAME]'])
+        template = template.replace("[COUNTRY]", data['[COUNTRY]'])
+        template = template.replace("[LABEL]", data['[LABEL]'].lower())
+        template = template.replace("[TEXT_A]", data['task-b-items']['text_a'].lower())
+        template = template.replace("[TEXT_B]", data['task-b-items']['text_b'].lower())
+        label = "yes" if data['task-b-items']['label'] == "correct" else "no"
+        return {"text": template, "label": label}
 
 class DatasetFactory:
 
