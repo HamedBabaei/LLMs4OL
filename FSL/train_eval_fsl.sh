67b9d61a48e2d7c8b5e3bbc1bb3babba3c76da34
Hamed Babaei <hamedbabaeigiglou@gmail.com>
2023-05-05 18:24:28 +0200

    [add] adding schema training to shell script

--- FSL/train_eval_fsl.sh
+++ FSL/train_eval_fsl.sh
@@ -1,6 +1,6 @@
 # #!/bin/bash
 
-label="nofinetuning"
+label="fewshot"
 device="cuda"
 task_a_templates=("template-1" "template-2" "template-3" "template-4" "template-5" "template-6" "template-7" "template-8")
 task_b_templates=("0" "1" "2" "3" "4" "5" "6" "7")
@@ -255,3 +255,55 @@ for kb_name in "${datasets[@]}"; do
     index=$((index+1))
   done
 done
+
+
+#-------------------------------------FLAN-T5-Large--------SCHEMA---------------------------------------------------------
+# running models for SchemaORG
+python trainer.py --kb_name="schema" --model_to_train="flan_t5_large" --num_train_epochs=5
+
+cd ..
+cd TaskB
+datasets=("schema")
+models=("schema_flan_t5_large")
+
+for kb_name in "${datasets[@]}"; do
+  index=8
+  for model_name in "${models[@]}"; do
+    log="results/$kb_name/$index-$kb_name-$model_name.$label.test.log.txt"
+    exec > $log
+    for template in "${task_b_templates[@]}"; do
+      echo "Running on dataset: $kb_name , model: $model_name, template: $template!"
+      python3 test.py --kb_name=$kb_name --model=$model_name --template=$template --device=$device
+      echo "Inference for  $model_name on template: $template  is done"
+      echo "-----------------------------------------------------------"
+    done
+    index=$((index+1))
+  done
+done
+
+cd ..
+cd FSLS
+
+#-------------------------------------FLAN-T5-XL--------SCHEMA---------------------------------------------------------
+# running models for SchemaORG
+python trainer.py --kb_name="schema" --model_to_train="flan_t5_xl" --num_train_epochs=5
+
+cd ..
+cd TaskB
+models=("schema_flan_t5_xl")
+
+for kb_name in "${datasets[@]}"; do
+  index=9
+  for model_name in "${models[@]}"; do
+    log="results/$kb_name/$index-$kb_name-$model_name.$label.test.log.txt"
+    exec > $log
+    for template in "${task_b_templates[@]}"; do
+      echo "Running on dataset: $kb_name , model: $model_name, template: $template!"
+      python3 test.py --kb_name=$kb_name --model=$model_name --template=$template --device=$device
+      echo "Inference for  $model_name on template: $template  is done"
+      echo "-----------------------------------------------------------"
+    done
+    index=$((index+1))
+  done
+done
+
